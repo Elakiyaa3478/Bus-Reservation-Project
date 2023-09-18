8ea5418ea44c2fb7302d59d6fcfe5eeb7bf53601
ElakiyaaM <90861190+Elakiyaa3478@users.noreply.github.com>
2023-09-19 03:41:17 +0530

    Create busresv.java

---
+++ busresv.java
@@ -0,0 +1,42 @@
+package jdbc;
+import java.util.Scanner;
+public class busresv {
+	public static void main(String args[])
+	{
+		BusDAO busdao=new BusDAO();
+		try
+		{
+			busdao.displayBusInfo();
+			int userOpt=1;
+			Scanner scanner =new Scanner(System.in);
+			while(userOpt==1)
+			{
+				System.out.println("Enter 1 to Book and 2 to exit");
+				userOpt=scanner.nextInt();
+				if(userOpt==1)
+				{
+					Booking booking =new Booking();
+					if(booking.isAvailable())
+					{
+						BookingDAO bookingdao=new BookingDAO();
+						bookingdao.addBooking(booking);
+						System.out.println("Your booking is confirmed");
+					}
+					else
+					{
+						System.out.println("Sorry!! Bus is full...! TRY ANOTHER BUS OR DATE");
+					}
+				}
+						
+						
+			}
+			scanner.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+	}
+
+}
